f2dd0148939a09857078b2a69e2c8eef80a3557d
moromoke01 <bellomoromoke2019@gmail.com>
2024-07-23 06:20:43 +0100

    initial commit

--- src/app/page.js
+++ src/app/page.js
@@ -38,13 +38,13 @@ export default function Home() {
 
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-200">
-      <div className="w-full w-3/6 p-5 m-10 bg-white rounded-lg shadow-md sm:w-screen md:w-2/3 lg: w-1/2 bg-purple-800">
-        <h1 className="text-2xl font-bold mb-4 text-center text-white"> My Todo List</h1>
+      <div className="w-full max-w-lg p-5 m-4 bg-purple-800 rounded-lg shadow-md sm:w-3/4 md:w-2/3 lg:w-1/2">
+        <h1 className="text-xl sm:text-2xl md:text-3xl font-bold mb-4 text-center text-white">My Todo List</h1>
         <TodoForm 
           initialData={currentTodo || {}} 
           onSave={handleSave} 
         />
-        <div className="mt-4 bg-white rounded-lg pt-10">
+        <div className="mt-4 bg-white rounded-lg p-4">
           {todos.map((todo) => (
             <TodoItem 
               key={todo.id} 
