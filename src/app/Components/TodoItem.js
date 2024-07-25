f83426f4de8199a048cb310836fb19d7ef5248c9
moromoke01 <bellomoromoke2019@gmail.com>
2024-07-25 19:34:33 +0100

    navigate to tododetail

--- src/app/Components/TodoItem.js
+++ src/app/Components/TodoItem.js
@@ -1,3 +1,5 @@
+
+'use client'
 import React from 'react';
 import Link from 'next/link';
 import { FaEdit } from "react-icons/fa";
@@ -5,14 +7,17 @@ import { RiDeleteBin6Fill } from "react-icons/ri";
 
 const TodoItem = ({ todo, onEdit, onDelete }) => {
   return (
-    
-    <div className="flex justify-between items-center p-4 border-b ">
+    <div className="flex justify-between items-center p-4 border-b">
       <Link href={`/todo/${todo.id}`}>
         <h3 className="text-lg px-5 font-semibold">{todo.title}</h3>
       </Link>
-      <div className='pl-5' >
-        <button onClick={() => onEdit(todo)} className="mr-2 text-blue-500"><FaEdit className="text-blue-600"/></button>
-        <button onClick={() => onDelete(todo.id)} className="text-red-500"><RiDeleteBin6Fill className="text-red-600"/></button>
+      <div className='pl-5'>
+        <button onClick={() => onEdit(todo)} className="mr-2 text-blue-500">
+          <FaEdit className="text-blue-600"/>
+        </button>
+        <button onClick={() => onDelete(todo.id)} className="text-red-500">
+          <RiDeleteBin6Fill className="text-red-600"/>
+        </button>
       </div>
     </div>
   );
---
+++ src/app/todo/[id]/page.js
@@ -0,0 +1,42 @@
+// src/app/todo/[id]/page.js
+'use client'
+import { useParams } from 'next/navigation';
+import { useEffect, useState } from 'react';
+
+const TodoDetail = () => {
+  const { id } = useParams();
+  const [todo, setTodo] = useState(null);
+
+  useEffect(() => {
+    const fetchTodo = async () => {
+      if (id) {
+        try {
+          const res = await fetch(`https://jsonplaceholder.typicode.com/todos/${id}`);
+          const data = await res.json();
+          setTodo(data);
+        } catch (error) {
+          console.error('Error fetching todo:', error);
+        }
+      }
+    };
+
+    fetchTodo();
+  }, [id]);
+
+  if (!todo) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <div className="w-3/6 mx-auto p-4 mt-10 bg-purple-800">
+      <h1 className="text-2xl font-bold mb-4 text-white text-center">Todo Detail</h1>
+      <div className="bg-white p-4 rounded-lg shadow-md">
+        <h2 className="text-xl font-semibold mb-2 ">Todo Title: <b>{todo.title}</b></h2>
+        <p>ID: {todo.id}</p>
+        <p>Completed: <span className='text-red-500 '><b>{todo.completed ? 'Yes' : 'No'}</b></span></p>
+      </div>
+    </div>
+  );
+};
+
+export default TodoDetail;
